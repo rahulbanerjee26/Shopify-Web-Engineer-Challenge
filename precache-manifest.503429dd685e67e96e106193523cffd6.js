63b9276669b9f48dc390fdd344d74227dc12e577
Rahul Banerjee <banerjeer2611@gmail.com>
2019-01-21 06:06:52 -0400

    Updates

---
+++ precache-manifest.503429dd685e67e96e106193523cffd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "514f3e79e09d3f82c2b3",
+    "url": "/Shopify-Web-Engineer-Challenge/static/js/runtime~main.514f3e79.js"
+  },
+  {
+    "revision": "a8037049404f98e7cea5",
+    "url": "/Shopify-Web-Engineer-Challenge/static/js/main.a8037049.chunk.js"
+  },
+  {
+    "revision": "3bba5fadb7d3b0f56462",
+    "url": "/Shopify-Web-Engineer-Challenge/static/js/1.3bba5fad.chunk.js"
+  },
+  {
+    "revision": "a8037049404f98e7cea5",
+    "url": "/Shopify-Web-Engineer-Challenge/static/css/main.784a6b2b.chunk.css"
+  },
+  {
+    "revision": "eb8a6306663081a3ea77c1ddc3d4882b",
+    "url": "/Shopify-Web-Engineer-Challenge/index.html"
+  }
+];
